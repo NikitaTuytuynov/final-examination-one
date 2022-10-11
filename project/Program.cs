16f5a4310be5b8eda9da47baa22c34faf978788b
Nikita <tumanstiv@mail.ru>
2022-10-11 19:57:34 +0300

    Third commit

---
+++ project/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Ведите слова разделяя их ТОЛЬКО пробелом: ");
+string s = Convert.ToString(Console.ReadLine());
+Console.WriteLine($"Исходная строка: {s}");
+void Print(string x)
+{
+    string[] subs = x.Split(' ');
+    foreach (var sub in subs)
+    {
+        int i = sub.Length;
+        if (i <= 3) Console.WriteLine($"-> {sub}");
+    }
+}
+Print(s);
